b327fc7e1f71f57f5780211ea639afa8d18f8d07
0Jaemin0 <jaemin2167@naver.com>
2025-07-08 00:27:30 +0900

    chore(HIGH-15): react query 설정 및 queryclientprovider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/hooks/queries/common/queryClient";
 import AppRouter from "./AppRouter";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppRouter />
+		<QueryClientProvider client={queryClient}>
+			<AppRouter />
+		</QueryClientProvider>
 	</StrictMode>
 );
