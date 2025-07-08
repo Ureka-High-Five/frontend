b5d8f4040e49c4b8b9837b25e025730e442fe48a
silverain <ekdldkaa@naver.com>
2025-07-08 16:43:02 +0900

    feat(HIGH-20): 전역 app에 bg-black 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 function App() {
 	return (
 		<div className="flex flex-col min-h-screen w-full">
-			<main className="flex-1 w-full max-w-[700px] mx-auto overflow-hidden">
+			<main className="flex-1 w-full max-w-[700px] mx-auto overflow-hidden bg-black">
 				<Outlet />
 			</main>
 		</div>
