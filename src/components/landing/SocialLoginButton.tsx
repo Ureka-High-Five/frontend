1c2e19645a0adcd4248b45a718475d381f521f8e
silverain <ekdldkaa@naver.com>
2025-07-09 10:58:37 +0900

    fix(HIGH-20): common Button 삭제 및 Button컴포로 마이그레이션

--- src/components/landing/SocialLoginButton.tsx
+++ src/components/landing/SocialLoginButton.tsx
@@ -1,23 +1,23 @@
 import { motion, AnimatePresence } from "framer-motion";
-import CommonButton from "@/components/common/CommonButton";
+import { Button } from "../ui/button";
 
 interface SocialLoginButtonProps {
   isVisible: boolean;
 }
 
-const MotionCommonButton = motion(CommonButton);
+const MotionCommonButton = motion(Button);
 
 const SocialLoginButton = ({ isVisible }: SocialLoginButtonProps) => (
   <AnimatePresence>
     {isVisible && (
       <MotionCommonButton
-        className="bg-[#FEE500]"
+        className="w-[90%] max-w-sm h-14 text-custom-black body-lg-dohyeon bg-[#FEE500]"
         initial={{ opacity: 0, y: 60 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 60 }}
-        transition={{ duration: 0.7, ease: "easeOut" }}
-        text="카카오로 시작하기"
-      />
+        transition={{ duration: 0.7, ease: "easeOut" }}>
+        카카오로 시작하기
+      </MotionCommonButton>
     )}
   </AnimatePresence>
 );
