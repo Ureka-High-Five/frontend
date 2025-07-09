085d2dae8d71446299abb01b8c90ce8b433741a8
silverain <ekdldkaa@naver.com>
2025-07-09 10:52:53 +0900

    feat(HIGH-14): detail페이지 파일 추가

---
+++ src/pages/ContentDetailPage.tsx
@@ -0,0 +1,9 @@
+const ContentDetailPage = () => {
+  return (
+    <div className="flex flex-col h-full w-full overflow-y-auto no-scrollbar">
+      <div>디테일페이지입니다.</div>
+    </div>
+  );
+};
+
+export default ContentDetailPage;
