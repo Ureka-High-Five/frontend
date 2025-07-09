098dd6f2b4d9916e9fa3bfbd684a5404b270c3d3
neungdong <mjkim9503@naver.com>
2025-07-09 18:00:43 +0900

    chore(HIGH-19): 배경색 수정

--- src/components/Home/HomeLayout.tsx
+++ src/components/Home/HomeLayout.tsx
@@ -3,7 +3,7 @@ import NavigationBar from "@/components/Nav/NavigationBar";
 
 const HomeLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen w-full bg-custom-gray">
+    <div className="flex flex-col min-h-screen w-full bg-custom-black">
       <main className="flex-1 w-full max-w-[360px] mx-auto overflow-hidden">
         <Outlet />
       </main>
