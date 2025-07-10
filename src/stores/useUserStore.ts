97facfebef1c107f625041d82257f5fe12214b55
0Jaemin0 <jaemin2167@naver.com>
2025-07-10 10:41:38 +0900

    feat(HIGH-23): 온보딩 제출을 위한 사용자 정보 store 구현

---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,57 @@
+import { create } from "zustand";
+
+interface UserData {
+  userId: number;
+  selectedContentIds: number[];
+  year: number;
+  gender: "male" | "female";
+  name: string;
+}
+
+interface UserState {
+  user: UserData;
+  setUserId: (id: number) => void;
+  setSelectedContentIds: (ids: number[]) => void;
+  setYear: (year: number) => void;
+  setGender: (gender: "male" | "female") => void;
+  setName: (name: string) => void;
+}
+
+const initialUser: UserData = {
+  userId: 0,
+  selectedContentIds: [],
+  year: 0,
+  gender: "male",
+  name: "",
+};
+
+const useUserStore = create<UserState>((set) => ({
+  user: initialUser,
+
+  setUserId: (id) =>
+    set((state) => ({
+      user: { ...state.user, userId: id },
+    })),
+
+  setSelectedContentIds: (ids) =>
+    set((state) => ({
+      user: { ...state.user, selectedContentIds: ids },
+    })),
+
+  setYear: (year) =>
+    set((state) => ({
+      user: { ...state.user, year },
+    })),
+
+  setGender: (gender) =>
+    set((state) => ({
+      user: { ...state.user, gender },
+    })),
+
+  setName: (name) =>
+    set((state) => ({
+      user: { ...state.user, name },
+    })),
+}));
+
+export default useUserStore;
