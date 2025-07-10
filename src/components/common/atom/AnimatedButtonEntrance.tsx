d30252aa27087d643df773a2d2402b8b0cdafc99
silverain <ekdldkaa@naver.com>
2025-07-10 10:55:39 +0900

    feat(HIGH-14): 애니메이션 아톰 분리

---
+++ src/components/common/atom/AnimatedButtonEntrance.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from "react";
+import { motion } from "framer-motion";
+
+interface Props {
+  children: ReactNode;
+}
+
+const AnimatedButtonEntrance = ({ children }: Props) => (
+  <motion.div
+    className="w-full flex justify-center items-center"
+    initial={{ opacity: 0, y: 60 }}
+    animate={{ opacity: 1, y: 0 }}
+    exit={{ opacity: 0, y: 60 }}
+    transition={{ duration: 0.7, ease: "easeOut" }}>
+    {children}
+  </motion.div>
+);
+
+export default AnimatedButtonEntrance;
