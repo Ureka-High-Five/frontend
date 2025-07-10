0973e4aaeff4298cffb383fa4e82206540371e22
neungdong <iyeseul877@gmail.com>
2025-07-10 14:59:10 +0900

    feat(HIGH-25): 추천 섹션 컴포넌트 구현

---
+++ src/components/Home/RecommendationSection.tsx
@@ -0,0 +1,36 @@
+import type { RecommendContent } from "@/types/RecommendContentsResponse";
+import ContentCard from "./ContentCard";
+
+interface RecommendationSectionProps {
+  title?: string;
+  customHeader?: React.ReactNode;
+  contents: RecommendContent[];
+}
+
+const RecommendationSection = ({
+  title,
+  customHeader,
+  contents,
+}: RecommendationSectionProps) => {
+  return (
+    <section className="mt-6">
+      {customHeader ? (
+        <div className="flex items-center gap-2 text-body-sm-pretendard mb-2">
+          {customHeader}
+        </div>
+      ) : (
+        <h3 className="font-semibold text-body-sm-pretendard mb-2">{title}</h3>
+      )}
+
+      <ul className="flex overflow-x-auto gap-2 no-scrollbar">
+        {contents.map((item) => (
+          <li key={item.contentId} className="flex-shrink-0 w-1/3">
+            <ContentCard posterUrl={item.posterUrl} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default RecommendationSection;
