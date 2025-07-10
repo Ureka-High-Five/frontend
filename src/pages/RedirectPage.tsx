26a1679c15c1837960e6a4061027c0f908f4d7d3
0Jaemin0 <jaemin2167@naver.com>
2025-07-10 21:26:53 +0900

    feat(HIGH-23): 카카오 로그인 로딩 UI 구현

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Loader2 } from "lucide-react";
 import useKakaoLoginMutation from "@/hooks/queries/auth/useKakaoLoginMutation";
 
 const RedirectPage = () => {
@@ -12,7 +13,17 @@ const RedirectPage = () => {
     }
   }, [mutatePostKakaoLogin]);
 
-  return <div>로딩 중</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center">
+        <Loader2 className="h-10 w-10 animate-spin text-custom-point mb-4" />
+        <p className="mb-1 heading-h1-dohyeon text-white">
+          카카오 계정으로 로그인 중입니다
+        </p>
+        <p className="body-md-dohyeon text-custom-gray">잠시만 기다려주세요.</p>
+      </div>
+    </div>
+  );
 };
 
 export default RedirectPage;
