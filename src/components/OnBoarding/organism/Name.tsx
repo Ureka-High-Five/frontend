e6d742059213529a2c54f450d4493f928cd65df6
0Jaemin0 <jaemin2167@naver.com>
2025-07-11 13:21:16 +0900

    feat(HIGH-28): 온보딩 페이지 이름 단계 구현

---
+++ src/components/OnBoarding/organism/Name.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import useUserStore from "@/stores/useUserStore";
+
+interface NameProps {
+  setStep: (step: "name" | "age" | "gender" | "content") => void;
+  isActive: boolean;
+}
+
+const Name = ({ setStep, isActive }: NameProps) => {
+  const name = useUserStore((state) => state.user.name);
+
+  return (
+    <section className="flex flex-col gap-9 w-[90%] max-w-sm">
+      <span
+        className={`flex h-14 items-center justify-center bg-white heading-h1-pretendard px-4 rounded-md ${
+          isActive ? "border-2 border-custom-point" : "border-none"
+        }`}>
+        {name}
+      </span>
+
+      {isActive && (
+        <Button
+          size="lg"
+          className="w-full bg-custom-point text-custom-black body-lg-dohyeon flex items-center justify-center"
+          onClick={() => setStep("age")}>
+          다음으로
+        </Button>
+      )}
+    </section>
+  );
+};
+
+export default Name;
