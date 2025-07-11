0c211194ddb154ca145b63e2c6686ce919777bcb
0Jaemin0 <jaemin2167@naver.com>
2025-07-11 13:44:52 +0900

    feat(HIGH-28): 온보팅 페이지 성별 선택 단계 구현

---
+++ src/components/OnBoarding/organism/GenderSelect.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import useUserStore from "@/stores/useUserStore";
+import { cn } from "@/utils/cn";
+
+interface GenderSelectProps {
+  setStep: (step: "name" | "age" | "gender" | "content") => void;
+}
+
+const GenderSelect = ({ setStep }: GenderSelectProps) => {
+  const gender = useUserStore((state) => state.user.gender);
+  const setGender = useUserStore((state) => state.setGender);
+
+  return (
+    <section className="flex flex-col gap-6 w-[90%] max-w-sm">
+      <Select
+        value={gender}
+        onValueChange={(value: "male" | "female") => setGender(value)}>
+        <SelectTrigger
+          className={cn(
+            "w-full bg-white text-custom-black rounded-md",
+            gender ? "border-none" : "border-2 border-custom-point"
+          )}>
+          <SelectValue placeholder="성별을 선택해주세요" />
+        </SelectTrigger>
+
+        <SelectContent>
+          <SelectItem value="male">남성</SelectItem>
+          <SelectItem value="female">여성</SelectItem>
+        </SelectContent>
+      </Select>
+
+      {!!gender && (
+        <Button
+          size="lg"
+          className="w-full bg-custom-point text-custom-black body-lg-dohyeon"
+          onClick={() => setStep("content")}>
+          다음으로
+        </Button>
+      )}
+    </section>
+  );
+};
+
+export default GenderSelect;
