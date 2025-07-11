c39800226c74acbb76405400d25a0030d1f92201
neungdong <iyeseul877@gmail.com>
2025-07-11 11:37:28 +0900

    chore(HIGH-25): style 제거

--- src/components/common/HorizontalScrollContainer.tsx
+++ src/components/common/HorizontalScrollContainer.tsx
@@ -9,12 +9,10 @@ const HorizontalScrollContainer = ({ children, className }: Props) => {
   return (
     <div
       className={cn(
+        scrollSnapType: "x mandatory"
         "flex gap-2 overflow-x-auto no-scrollbar scroll-smooth snap-x",
         className
-      )}
-      style={{
-        scrollSnapType: "x mandatory",
-      }}>
+      )}>
       {children}
     </div>
   );
