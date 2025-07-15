e92776017e0f9ebbd5ed4488c8ab4a1cca8c142f
0Jaemin0 <jaemin2167@naver.com>
2025-07-15 19:53:45 +0900

    refactor(HIGH-28): 온보딩 콘텐츠 쿼리에서 불필요한 캐시 옵션 제거

--- src/hooks/queries/onboarding/useOnBoardingContentQuery.ts
+++ src/hooks/queries/onboarding/useOnBoardingContentQuery.ts
@@ -6,8 +6,6 @@ const useOnBoardingContentQuery = () => {
   const { data: onBoardingContent } = useQuery<OnBoardingContent[]>({
     queryKey: ["onBoardingContent"],
     queryFn: () => getOnBoardingContents(),
-    gcTime: 10 * 60 * 1000,
-    staleTime: 10 * 60 * 1000,
   });
 
   return { onBoardingContent };
