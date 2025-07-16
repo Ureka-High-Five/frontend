a72ddd8e7e6838b3f99a5b77d9b18eb3b4dd4795
neungdong <mjkim9503@naver.com>
2025-07-16 13:21:21 +0900

    hotfix: AppRouter 경로 변경

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { PATH } from "@/constants/path";
 import ContentDetailPage from "@/pages/ContentDetailPage";
-import HomePage from "@/pages/Home/HomePage";
+import HomePage from "@/pages/HomePage";
 import LandingPage from "@/pages/LandingPage";
 import OnBoardingPage from "@/pages/OnBoardingPage";
 import RedirectPage from "@/pages/RedirectPage";
