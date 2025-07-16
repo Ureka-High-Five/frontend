931e4282d2fbb1a4e18c2659923c5b496c3cb9cb
neungdong <iyeseul877@gmail.com>
2025-07-16 13:57:12 +0900

    hotfix: 빌드 에러 해결

--- src/types/RecommendContentsResponse.d.ts
+++ src/types/RecommendContentsResponse.d.ts
@@ -24,7 +24,7 @@ export interface RecommendCuration {
 
 export interface RecommendContentsResponse {
   mainRecommend: MainRecommend;
-  recommendContents: RecommendContent[];
+  personalRecommends: RecommendContent[];
   recommendGenreContents: RecommendGenreContent;
   recommendSecondGenreContents: RecommendGenreContent;
   recommendCuration: RecommendCuration;
