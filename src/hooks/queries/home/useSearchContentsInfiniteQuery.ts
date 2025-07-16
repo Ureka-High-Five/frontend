1726aa7c41e42502461c70ddac85635ccac3a1fd
neungdong <iyeseul877@gmail.com>
2025-07-16 20:51:15 +0900

    feat(HIGH-30): 검색 콘텐츠 무한 스크롤 쿼리 훅 구현

---
+++ src/hooks/queries/home/useSearchContentsInfiniteQuery.ts
@@ -0,0 +1,16 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getSearchContents } from '@/apis/home/getSearchContents'; 
+import type { GetSearchContentsResponse } from '@/types/search'; 
+
+export const useSearchContentsInfiniteQuery = (input: string) => {
+  return useInfiniteQuery<GetSearchContentsResponse>({
+    queryKey: ['search', input],
+    queryFn: ({ pageParam = undefined }) =>
+    getSearchContents({ input, cursor: pageParam }),
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNext ? lastPage.nextCursor : undefined,
+    initialPageParam: undefined,
+    enabled: !!input,
+    keepPreviousData: true,
+  });
+};
