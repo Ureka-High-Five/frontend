428eb518072f961e5eccbf09471b4b7d6b01e16b
silverain <ekdldkaa@naver.com>
2025-07-17 10:06:11 +0900

    chore(HIGH-27): mock data update

--- src/pages/ShortsPage.tsx
+++ src/pages/ShortsPage.tsx
@@ -5,7 +5,7 @@ const shorts = [
     contentId: "001",
     contentTitle: "센과 치히로의 행방불명",
     shortsUrl:
-      "https://highfive-stream-demo.s3.ap-northeast-2.amazonaws.com/WICKED.mp4",
+      "https://dev-leadme.s3.ap-northeast-2.amazonaws.com/video_segment/1022789_%EC%9D%B8%EC%82%AC%EC%9D%[…]%AC%EC%9D%B4%EB%93%9C_%EC%95%84%EC%9B%83_2.m3u8",
     shortThumbnail: "/images/poster1.webp",
   },
   {
@@ -18,7 +18,8 @@ const shorts = [
   {
     contentId: "003",
     contentTitle: "이웃집 토토로",
-    shortsUrl: "/videos/reel3.m3u8",
+    shortsUrl:
+      "https://dev-leadme.s3.ap-northeast-2.amazonaws.com/shorts_segment/950387_shorts/950387_shorts.m3u8",
     shortThumbnail: "/images/poster3.webp",
   },
   {
