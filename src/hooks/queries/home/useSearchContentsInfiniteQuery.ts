b8f390a46f80ac4d0bc0992f0dc04cb251eda5e0
0Jaemin0 <jaemin2167@naver.com>
2025-07-17 15:05:18 +0900

    refactor(HIGH-32): 검색 컨텐츠 데이터 평탄화 및 반환 구조 개선

--- src/hooks/queries/home/useSearchContentsInfiniteQuery.ts
+++ src/hooks/queries/home/useSearchContentsInfiniteQuery.ts
@@ -1,15 +1,25 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { getSearchContents } from '@/apis/home/getSearchContents'; 
-import type { GetSearchContentsResponse } from '@/types/search'; 
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getSearchContents } from "@/apis/home/getSearchContents";
+import type { GetSearchContentsResponse } from "@/types/search";
 
 export const useSearchContentsInfiniteQuery = (input: string) => {
-  return useInfiniteQuery<GetSearchContentsResponse>({
-    queryKey: ['search', input],
-    queryFn: ({ pageParam = undefined }) =>
-    getSearchContents({ input, cursor: pageParam as number | undefined }),
-    getNextPageParam: (lastPage) =>
-      lastPage.hasNext ? lastPage.nextCursor : undefined,
-    initialPageParam: undefined,
-    enabled: !!input,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<GetSearchContentsResponse>({
+      queryKey: ["search", input],
+      queryFn: ({ pageParam = undefined }) =>
+        getSearchContents({ input, cursor: pageParam as number | undefined }),
+      getNextPageParam: (lastPage) =>
+        lastPage.hasNext ? lastPage.nextCursor : undefined,
+      initialPageParam: undefined,
+      enabled: !!input,
+    });
+
+  const searchContents = data?.pages.flatMap((page) => page.items) ?? [];
+
+  return {
+    searchContents,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  };
 };
