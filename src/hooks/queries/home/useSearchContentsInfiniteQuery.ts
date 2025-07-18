99940e893543ccfc4a4fbc0f19dccb393b51d04d
0Jaemin0 <jaemin2167@naver.com>
2025-07-18 11:37:34 +0900

    feat(HIGH-32): useSearchContentsInfiniteQuery 훅에서 size를 prop으로 받을 수 있도록 수정

--- src/hooks/queries/home/useSearchContentsInfiniteQuery.ts
+++ src/hooks/queries/home/useSearchContentsInfiniteQuery.ts
@@ -2,16 +2,21 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { getSearchContents } from "@/apis/home/getSearchContents";
 import type { GetSearchContentsResponse } from "@/types/search";
 
-export const useSearchContentsInfiniteQuery = (input: string) => {
+export const useSearchContentsInfiniteQuery = (input: string, size = 10) => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery<GetSearchContentsResponse>({
-      queryKey: ["search", input],
+      queryKey: ["search", input, size],
       queryFn: ({ pageParam = undefined }) =>
-        getSearchContents({ input, cursor: pageParam as number | undefined }),
+        getSearchContents({
+          input,
+          cursor: pageParam as number | undefined,
+          size,
+        }),
       getNextPageParam: (lastPage) =>
         lastPage.hasNext ? lastPage.nextCursor : undefined,
       initialPageParam: undefined,
       enabled: !!input,
+      staleTime: 60 * 1000,
     });
 
   const searchContents = data?.pages.flatMap((page) => page.items) ?? [];
