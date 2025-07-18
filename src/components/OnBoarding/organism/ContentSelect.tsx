9a49335c9e3fd0505cb015ecdb0a75f26d98b698
0Jaemin0 <jaemin2167@naver.com>
2025-07-18 13:39:32 +0900

    feat(HIGH-32): 온보딩 컨텐츠 선택 단계 컨텐츠 검색 기능 구현

--- src/components/OnBoarding/organism/ContentSelect.tsx
+++ src/components/OnBoarding/organism/ContentSelect.tsx
@@ -1,6 +1,9 @@
+import { Loader2 } from "lucide-react";
 import OnBoardingContentCard from "@/components/OnBoarding/molecule/OnBoardingContentCard";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { REQUIRED_COUNT } from "@/constants/onBoarding";
+import { useSearchContent } from "@/hooks/onboarding/useSearchContent";
 import type { OnBoardingContent } from "@/types/content";
 
 interface ContentSelectProps {
@@ -16,11 +19,35 @@ const ContentSelect = ({
   toggleSelect,
   onSubmitOnBoarding,
 }: ContentSelectProps) => {
+  const {
+    searchInput,
+    setSearchInput,
+    searchKeyword,
+    searchContents,
+    isFetchingNextPage,
+    scrollContainerRef,
+    rootRef,
+    targetRef,
+  } = useSearchContent(6);
+  const displayContents = searchKeyword.length > 0 ? searchContents : contents;
+
   return (
     <>
-      <div className="w-[90%] max-w-sm overflow-y-auto no-scrollbar max-h-[calc(100dvh-312px)]">
+      <Input
+        type="text"
+        placeholder="좋아하는 작품을 검색해보세요"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        className="w-[90%] max-w-sm text-white placeholder:text-gray-500 bg-white text-custom-black h-10"
+      />
+      <div
+        ref={(el) => {
+          rootRef.current = el;
+          scrollContainerRef.current = el;
+        }}
+        className="w-[90%] max-w-sm overflow-y-auto no-scrollbar flex-1">
         <ul className="grid grid-cols-3 gap-10">
-          {contents.map((content) => (
+          {displayContents.map((content) => (
             <OnBoardingContentCard
               key={content.contentId}
               contentId={content.contentId}
@@ -32,6 +59,13 @@ const ContentSelect = ({
             />
           ))}
         </ul>
+
+        {searchKeyword && <div ref={targetRef} className="h-6" />}
+        {isFetchingNextPage && (
+          <div className="flex items-center w-full justify-center">
+            <Loader2 className="h-10 w-10 animate-spin text-custom-point mb-4" />
+          </div>
+        )}
       </div>
       <Button
         size="lg"
