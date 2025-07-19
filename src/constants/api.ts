c348500345677630819d213143a1d93cc6072330
neungdong <iyeseul877@gmail.com>
2025-07-19 12:03:36 +0900

    feat(HIGH-36): 엔드포인트 네이밍 통일

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -10,7 +10,7 @@ export const END_POINTS = {
   PREVIEW_VIDEO: (contentId: number) => `/content/${contentId}/video`,
   CONTENT: "content",
   CONTENT_REVIEW: "content/review",
-  SEARCH_CONTENT: "/content/search",
+  CONTENT_SEARCH: "/content/search",
   SHORTS: "/shorts",
   SHORTS_LIKE: "shorts/like",
   SHORTS_DISLIKE: "shorts/dislike",
