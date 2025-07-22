7c741dbb377525051300e19f8f2b970d5cecc395
0Jaemin0 <jaemin2167@naver.com>
2025-07-22 10:23:06 +0900

    feat(HIGH-37): 마이페이지 사용자 프로필 정보 컴포넌트 구현

---
+++ src/components/My/molecule/Profile.tsx
@@ -0,0 +1,34 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import type { UserInformation } from "@/types/user";
+
+interface ProfileProps {
+  userInformation: UserInformation;
+}
+
+const Profile = ({ userInformation }: ProfileProps) => {
+  return (
+    <section className="flex items-center gap-4">
+      <Avatar className="w-14 h-14">
+        <AvatarImage src={userInformation.profileUrl} alt="프로필 이미지" />
+        <AvatarFallback>{userInformation.name[0]}</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col">
+        <div className="flex items-center gap-2">
+          <h2 className="heading-h2-pretendard text-white">
+            {userInformation.name}님
+          </h2>
+          {userInformation.role === "EDITOR" && (
+            <span className="px-2 border border-custom-point body-md-pretendard rounded-full text-custom-point">
+              큐레이션
+            </span>
+          )}
+        </div>
+        <p className="body-md-pretendard text-custom-gray">
+          {userInformation.email}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
