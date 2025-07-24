791d7563c3daa07b48222545a45a72fbcb11b17a
silverain <ekdldkaa@naver.com>
2025-07-24 16:12:34 +0900

    feat: content undefined방어처리

--- src/components/admin/content-modal.tsx
+++ src/components/admin/content-modal.tsx
@@ -54,9 +54,7 @@ export function ContentModal({ isOpen, onClose, content }: ContentModalProps) {
   const [genreInput, setGenreInput] = useState("");
   const [actorInput, setActorInput] = useState("");
 
-  const { content: contentDetail } = useContentDetailQuery(
-    String(content?.contentId)
-  );
+  const { content: contentDetail } = useContentDetailQuery(content?.contentId);
   const { mutatePostContent, isPosting } = usePostContentMutation();
   const { mutatePatchContent } = usePatchContent();
 
