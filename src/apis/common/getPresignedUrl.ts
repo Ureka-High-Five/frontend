0c2202f1eb3222664a14c02e0a9b95ec8309ab0f
0Jaemin0 <jaemin2167@naver.com>
2025-07-24 13:23:04 +0900

    feat(HIGH-37): presigned URL 기반 이미지 업로드 hook 구현

--- src/apis/common/getPresignedUrl.ts
+++ src/apis/common/getPresignedUrl.ts
@@ -1,7 +1,12 @@
 import { axiosInstance } from "@/apis/axiosInstance";
 import { END_POINTS } from "@/constants/api";
 
-const getPresignedUrl = async () => {
+interface PresignedUrlResponse {
+  presignedUrl: string;
+  imageUrl: string;
+}
+
+const getPresignedUrl = async (): Promise<PresignedUrlResponse> => {
   const response = await axiosInstance.get(END_POINTS.PRESIGNEDURL);
 
   return response.data.content;
