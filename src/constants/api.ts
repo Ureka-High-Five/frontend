49b9a7120e1a683505e3b0e0817f22b129eeb9e4
0Jaemin0 <127086869+0Jaemin0@users.noreply.github.com>
2025-07-25 11:46:14 +0900

    fix(HIGH-40): shortsId 타입 변경

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -20,7 +20,7 @@ export const END_POINTS = {
   MY_CURATION: "curation/me",
   CURATION: "curation",
   PRESIGNEDURL: "s3/presignedUrl",
-  SHORTS_COMMENT_TIMELINE: (shortsId: string) => `/shorts/${shortsId}/comments`,
+  SHORTS_COMMENT_TIMELINE: (shortsId: number) => `/shorts/${shortsId}/comments`,
   WATCH_LOG: "content/watch-log",
 } as const;
 
