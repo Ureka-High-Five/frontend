aa4e73132ddbdb736df440fcc2d49c2081c6c175
silverain <ekdldkaa@naver.com>
2025-07-28 22:09:08 +0900

    fix(HIGH-52): d-vh전역 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen w-full bg-gray-500">
+    <div className="flex flex-col h-dvh w-full bg-gray-500">
       <main className="flex-1 w-full max-w-[768px] mx-auto bg-custom-black overflow-hidden">
         <Outlet />
       </main>
