cfead1ef89c108e26a2f1e1174a52f613772752e
silverain <ekdldkaa@naver.com>
2025-07-28 19:31:04 +0900

    feat(HIGH-52): absolute전환을 통한 위치 조정

--- src/components/shorts/ShortsLayout.tsx
+++ src/components/shorts/ShortsLayout.tsx
@@ -32,7 +32,7 @@ export default function ShortsLayout({
     <div
       ref={rootRef}
       className="relative w-full h-screen overflow-y-scroll snap-y snap-mandatory">
-      <div className="fixed top-4 left-2 z-10 text-white">
+      <div className="absolute top-4 left-4 z-10 text-white">
         <Button variant="ghost">
           <X className="w-6 h-6" />
         </Button>
