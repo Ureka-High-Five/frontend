05fb8ac590d433c280797e08e0f00d16f722ca5a
0Jaemin0 <jaemin2167@naver.com>
2025-07-29 13:46:16 +0900

    feat(HIGH-50): 컨텐츠 등록을 위한 장르 목록 및 국가 코드 상수 정의

---
+++ src/constants/admin.ts
@@ -0,0 +1,19 @@
+export const GENRE_LIST = [
+  "공포",
+  "액션",
+  "스릴러",
+  "미스터리",
+  "코미디",
+  "애니메이션",
+  "SF",
+  "다큐멘터리",
+  "판타지",
+  "드라마",
+  "모험",
+  "로맨스",
+  "범죄",
+  "역사",
+  "전쟁",
+] as const;
+
+export const COUNTRY_CODE_LIST = ["US", "KR", "JP", "CN"] as const;
