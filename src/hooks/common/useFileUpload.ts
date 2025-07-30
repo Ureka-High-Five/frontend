42e48bf47331acb195d782a5e981743d5d5a6ac1
0Jaemin0 <jaemin2167@naver.com>
2025-07-31 00:44:36 +0900

    feat(HIGH-57): 콘텐츠 업로드용 uploadContentFile 함수 추가

--- src/hooks/common/useFileUpload.ts
+++ src/hooks/common/useFileUpload.ts
@@ -1,6 +1,13 @@
+import getContentPresignedUrl from "@/apis/common/getContentPresignedUrl";
 import getCurationPresignedUrl from "@/apis/common/getCurationPresignedUrl";
 import useFileUploadMutation from "@/hooks/queries/common/useFileUploadMutation";
 import { makeToast } from "@/utils/makeToast";
+
+interface UploadContentFiles {
+  imageFile: File;
+  shortsFile: File;
+  videoFile: File;
+}
 
 const useFileUpload = () => {
   const { mutateFileUpload } = useFileUploadMutation();
@@ -21,8 +28,47 @@ const useFileUpload = () => {
     }
   };
 
+  const uploadContentFile = async ({
+    imageFile,
+    shortsFile,
+    videoFile,
+  }: UploadContentFiles) => {
+    try {
+      const {
+        imagePresignedUrl,
+        imageUrl,
+        shortsPresignedUrl,
+        shortsUrl,
+        videoPresignedUrl,
+        videoUrl,
+      } = await getContentPresignedUrl();
+      await Promise.all([
+        mutateFileUpload({ presignedUrl: imagePresignedUrl, file: imageFile }),
+        mutateFileUpload({
+          presignedUrl: shortsPresignedUrl,
+          file: shortsFile,
+        }),
+        mutateFileUpload({ presignedUrl: videoPresignedUrl, file: videoFile }),
+      ]);
+
+      return {
+        imageUrl,
+        shortsUrl,
+        videoUrl,
+      };
+    } catch (error) {
+      makeToast(
+        "파일 업로드 중 오류가 발생했어요. 잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+
+      return null;
+    }
+  };
+
   return {
     uploadCurationFile,
+    uploadContentFile,
   };
 };
 
