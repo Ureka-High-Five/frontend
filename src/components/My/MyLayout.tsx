f857180037a0547dc79d3cd69ce8a3984a078134
neungdong <iyeseul877@gmail.com>
2025-07-31 17:22:32 +0900

    refactor(HIGH-60): 홈 및 마이 컨텐츠 가리는거 수정

--- src/components/My/MyLayout.tsx
+++ src/components/My/MyLayout.tsx
@@ -11,7 +11,7 @@ interface MyLayoutProps {
 const MyLayout = ({ userInformation }: MyLayoutProps) => {
   return (
     <div className="relative flex flex-col h-screen-mobile">
-      <div className="flex flex-col flex-1 overflow-y-auto px-8 md:px-10 pt-3 gap-8">
+      <div className="flex flex-col flex-1 overflow-y-auto pb-20 px-8 md:px-10 pt-3 gap-8">
         <header>
           <img src={logoImg} alt="logo" className="h-8" />
         </header>
