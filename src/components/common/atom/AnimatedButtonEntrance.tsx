54c3c40d23ef88650251bb5aaa0c0f7622fd1a62
0Jaemin0 <jaemin2167@naver.com>
2025-07-31 13:06:16 +0900

    style(HIGH-57): 랜딩 페이지 로그인 버튼 animation 위치 수정

--- src/components/common/atom/AnimatedButtonEntrance.tsx
+++ src/components/common/atom/AnimatedButtonEntrance.tsx
@@ -8,9 +8,9 @@ interface Props {
 const AnimatedButtonEntrance = ({ children }: Props) => (
   <motion.div
     className="w-full flex justify-center items-center"
-    initial={{ opacity: 0, y: 60 }}
+    initial={{ opacity: 0, y: 30 }}
     animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0, y: 60 }}
+    exit={{ opacity: 0, y: 30 }}
     transition={{ duration: 0.7, ease: "easeOut" }}>
     {children}
   </motion.div>
