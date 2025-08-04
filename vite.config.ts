e92c197be35d3b385a736d6530ecb00c0263cf03
0Jaemin0 <jaemin2167@naver.com>
2025-08-04 13:18:47 +0900

    chore(HIGH-65): manualChunks로 vendor 분리

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,19 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     visualizer({
-      filename: "bundle-report.html", // 분석 결과 파일
-      open: true, // 빌드 완료 후 자동 브라우저 열기
-      template: "treemap", // treemap 방식 시각화
-      gzipSize: true, // gzip 사이즈 표시
-      brotliSize: true, // brotli 사이즈 표시
+      filename: "bundle-report.html",
+      open: true,
+      template: "treemap",
+      gzipSize: true,
+      brotliSize: true,
     }),
   ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined, // chunk 분석을 명확하게 보이도록 설정
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
       },
     },
   },
