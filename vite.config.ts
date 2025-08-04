13f81e8c2f684d98b82823751e6c5767c2e4ac06
0Jaemin0 <jaemin2167@naver.com>
2025-08-04 11:28:02 +0900

    feat(HIGH-65): 번들 시각화 라이브러리 설치 및 lodash 라이브러리 부분 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    visualizer({
+      filename: "bundle-report.html", // 분석 결과 파일
+      open: true, // 빌드 완료 후 자동 브라우저 열기
+      template: "treemap", // treemap 방식 시각화
+      gzipSize: true, // gzip 사이즈 표시
+      brotliSize: true, // brotli 사이즈 표시
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // chunk 분석을 명확하게 보이도록 설정
+      },
+    },
+  },
 });
